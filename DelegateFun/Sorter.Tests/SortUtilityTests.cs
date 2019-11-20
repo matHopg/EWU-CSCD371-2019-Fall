7f15ce47462fda7d73207b7e6f3ff1500df3fc24
Matthew <mghop101@gmail.com>
2019-11-19 19:16:33 -0800

    Completed Work

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -1,13 +1,57 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Sorter.Tests
 {
     [TestClass]
     public class SortUtilityTests
     {
-        [TestMethod]
-        public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
+        [DataTestMethod]
+        [DataRow(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 },
+            new[] { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 2, 1, 0 })]
+        public void SortUtility_ShouldAscendAbove3_ThenDescend(int[] array, int[] expected)
         {
+            var sortUtility = new SortUtility();
+            sortUtility.InsertionSort(array, (first, second) => { if (first < 3 || second < 3) { return first > second; } return first < second; });
+            CollectionAssert.AreEqual(expected, array);
+
         }
+        [DataTestMethod]
+        [DataRow(new[] { 3, 4, 2, 5, 6, 7, 8, 9, 1, 0 }, new[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})]
+        [DataRow(new[] { 0,0,0,2,1}, new[] { 0,0,0,1,2})]
+        public void SortUtility_SortAscending(int[] array, int[] expected)
+        {
+            var sortUtility = new SortUtility();
+            Comparison comparison = delegate (int first, int second)
+            {
+                return first < second;
+            };
+            sortUtility.InsertionSort(array, comparison);
+            CollectionAssert.AreEqual(expected, array);
+        }
+
+        [DataTestMethod]
+        [DataRow(new[] { 0 })]
+        [DataRow(null)]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SortUtility_GivenNull_ExcpetionThrown(int[] array)
+        {
+            var sortUtility = new SortUtility();
+            if(array is null)
+            {
+                sortUtility.InsertionSort(null, (first, second) => first > second);
+            }
+            sortUtility.InsertionSort(array, null);
+        }
+        [DataTestMethod]
+        [DataRow(new[] { 1,4,2,3}, new[] { 4, 3, 2, 1 })]
+        public void SortUtility_SortDescendingAlphabetical_UsingLambda(int[] array, int[] expected)
+        {
+            var sortUtility = new SortUtility();
+            sortUtility.InsertionSort(array, (int first, int second) => { return first.ToString().CompareTo(second.ToString()) > 0; });
+            CollectionAssert.AreEqual(array, expected);
+        }
+
+
     }
 }
--- DelegateFun/Sorter/SortUtility.cs
+++ DelegateFun/Sorter/SortUtility.cs
@@ -2,9 +2,35 @@
 
 namespace Sorter
 {
+    public delegate bool Comparison(int first, int second);
+
     public class SortUtility
     {
         // Sort method should be implemented here
         // It should accept an int[] and a delegate you define that performs the actual comparison
+        public void InsertionSort(int[] array, Comparison comparison)
+        {
+            if (array is null) throw new ArgumentNullException($"{nameof(array)} cannot be null");
+            if (comparison is null) throw new ArgumentNullException($"{nameof(comparison)} cannot be null");
+            for(var i =1; i<array.Length; i++)
+            {
+                var currentValue = array[i];
+                var tempValue = 0;
+                for(var x = i-1; x>= 0 && tempValue != 1;)
+                {
+                    if(comparison(currentValue, array[x]))
+                    {
+                        array[x + 1] = array[x];
+                        x--;
+                        array[x + 1] = currentValue;
+                    }
+                    else
+                    {
+                        tempValue = 1;
+                    }
+                }
+
+            }
+        }
     }
 }
